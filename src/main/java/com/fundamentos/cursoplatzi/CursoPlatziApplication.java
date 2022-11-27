c87e938b7c69e12e91c9b67322c1622b0696f924
Fabian-Ojeda <fabian.ojeda01@uptc.edu.co>
2022-11-27 12:49:38 -0500

    Para verificar que se pueden subir cambios al repo en github

---
+++ src/main/java/com/fundamentos/cursoplatzi/CursoPlatziApplication.java
@@ -0,0 +1,14 @@
+package com.fundamentos.cursoplatzi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoPlatziApplication {
+
+    public static void main(String[] args) {
+        //COmentario para subir a github
+        SpringApplication.run(CursoPlatziApplication.class, args);
+    }
+
+}
